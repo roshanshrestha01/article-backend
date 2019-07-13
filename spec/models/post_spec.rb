5870ddff1d4cc8351b96e227faef679a1c64c141
Roshan Shrestha <roshanshrestha01@gmail.com>
2019-07-13 22:01:50 +0545

    RSpec validation for required fields in user, post and comments

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   it { should belong_to(:user) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:link) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should have_many(:post) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
 end
