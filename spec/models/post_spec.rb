5516edc750d40ae09499f385fad1672e1ac1e1aa
Roshan Shrestha <roshanshrestha01@gmail.com>
2019-07-14 08:47:38 +0545

    RSpec test for post having many comments

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   it { should belong_to(:user) }
+  it { should have_many(:comment) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:link) }
 end
