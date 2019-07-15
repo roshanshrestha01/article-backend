69269a8b9566c8fb6c00a3d065e7f65d3a7ea075
Roshan Shrestha <roshanshrestha01@gmail.com>
2019-07-15 09:06:51 +0545

    Login post to get token

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,9 +1,8 @@
 class V1::SessionsController < ApplicationController
-    skip_before_action :authorize_request, only: :authenticate
+    skip_before_action :authorize_request, only: :create
 
-    def authenticate
-        auth_token =
-        AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+    def create
+        auth_token = AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
         json_response(auth_token: auth_token)
     end
 
